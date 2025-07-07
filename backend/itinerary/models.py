16847c0a4ac328dfcc04926de2bc137af493ff8b
fookang <fookang3@gmail.com>
2025-07-07 20:01:39 +0800

    Added views and models for itinerary

--- backend/itinerary/models.py
+++ backend/itinerary/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Itinery(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete = models.CASCADE
+        )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class ItineraryDay(models.Model):
+    itinerary = models.ForeignKey(
+        Itinery, 
+        on_delete = models.CASCADE)
+    title = models.CharField(max_length=50)
+    date = models.DateField()
+    
+    def __str__(self):
+        return f"{self.itinery.title} - {self.date}"
+    
+class Activity(models.Model):
+    itineraryday = models.ForeignKey(ItineraryDay, on_delete=models.CASCADE)
+    time = models.TimeField()
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.title} - {self.time}"
--- backend/itinerary/views.py
+++ backend/itinerary/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from .models import Itinery, ItineryDay, Activity
 
 # Create your views here.
+class ItineraryListCreate(generics.ListCreateAPIView):
+    # serializer_class = ItinerarySerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Itinery.objects.filter(user=self.request.user)
+    
+    
+class ItineraryDayListCreate(generics.ListCreateAPIView):
+    # serializer_class = ItineraryDaySerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return ItineryDay.objects.filter(itinerary=self.kwargs.get("itinerary_id"))
+
+
+    
+class ActivityListCreate(generics.CreateAPIView):
+    # serializer_class = ActivitySerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Activity.objects.filter(itineraryday=self.kwargs.get("day_id"))
