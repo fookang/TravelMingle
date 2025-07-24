039e5d643ecf9b210d71ee1c33e6e12d300c8031
fookang <fookang3@gmail.com>
2025-07-25 00:59:32 +0800

    Change formatDate to be non timezone sensitive and Added formateAsYYYYMMDD

--- frontend/constants/fomatDate.js
+++ frontend/constants/fomatDate.js
@@ -1,8 +1,27 @@
-export const formatDate = (datestr) => {
-  const date = new Date(datestr);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = date.toLocaleString("default", { month: "short" });
-  const year = date.getFullYear();
+export const formatDate = (dateStr) => {
+  const [year, month, day] = dateStr.split("-");
 
-  return `${day} ${month} ${year}`;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  return `${day} ${months[parseInt(month, 10) - 1]} ${year}`;
+};
+
+export const formatAsYYYYMMDD = (dateObj) => {
+  const yyyy = dateObj.getFullYear();
+  const mm = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const dd = String(dateObj.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
 };
