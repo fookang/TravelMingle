c94a676916c6f80c6d5718a49874539098546e9e
fookang <fookang3@gmail.com>
2025-07-28 21:30:34 +0800

    Breaking DayForm into Components

--- frontend/app/(app)/itineraries/[id]/days/createDay.jsx
+++ frontend/app/(app)/itineraries/[id]/days/createDay.jsx
@@ -1,33 +1,18 @@
-import {
-  Alert,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { Alert, StyleSheet } from "react-native";
 import api from "../../../../../services/api";
 import { useRouter, useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../../../components/Header";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import {
-  formatDate,
-  displayDate,
-  parseYYYYMMDD,
-} from "../../../../../constants/fomatDate";
+
+import DayForm from "../../../../components/DayForm";
 
 const createDay = () => {
   const router = useRouter();
   const { id, start_date, end_date } = useLocalSearchParams();
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState(parseYYYYMMDD(start_date));
-  const [dateButton, setDateButton] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [showTitleError, setShowTitleError] = useState(false);
 
-  const handleCreate = async () => {
+  const handleCreate = async ({ title, date }) => {
     try {
       setLoading(true);
       const response = await api.post(`itinerary/${id}/days/`, {
@@ -50,68 +35,14 @@ const createDay = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Create new Itinerary Day" />
-      <View style={styles.content}>
-        <View style={styles.input}>
-          <Text>
-            Title: <Text style={styles.required}>*</Text>
-          </Text>
-          <TextInput
-            placeholder="Title"
-            value={title}
-            onChangeText={(text) => {
-              setTitle(text);
-              if (showTitleError) setShowTitleError(false);
-            }}
-            style={styles.inputText}
-          />
-        </View>
-
-        <View>
-          <TouchableOpacity onPress={() => setDateButton(true)}>
-            <View style={styles.input}>
-              <Text style={styles.label}>Date:</Text>
-              <Text style={styles.dateValue}>
-                {displayDate(date) || "Today"}
-              </Text>
-            </View>
-          </TouchableOpacity>
-          {dateButton && (
-            <DateTimePicker
-              value={date}
-              mode="date"
-              display="default"
-              minimumDate={parseYYYYMMDD(start_date)}
-              maximumDate={parseYYYYMMDD(end_date)}
-              onChange={(event, selectedDate) => {
-                if (selectedDate) {
-                  setDate(selectedDate);
-                  setDateButton(false);
-                }
-              }}
-            />
-          )}
-        </View>
-
-        {showTitleError && !title.trim() && (
-          <Text style={styles.errorText}>Title is required.</Text>
-        )}
-
-        <TouchableOpacity
-          onPress={() => {
-            if (!title.trim()) {
-              setShowTitleError(true);
-              return;
-            }
-            handleCreate();
-          }}
-          disabled={loading}
-          style={[styles.button, loading && styles.buttonDisabled]}
-        >
-          <Text style={styles.buttonText}>
-            {loading ? "Submitting" : "Submit"}
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <DayForm
+        handleCreate={handleCreate}
+        start_date={start_date}
+        end_date={end_date}
+        loading={loading}
+        title=""
+        date={undefined}
+      />
     </SafeAreaView>
   );
 };
@@ -122,51 +53,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 10,
-  },
-  content: {
-    marginHorizontal: 20,
-    marginTop: 10,
-  },
-  input: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  inputText: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 10,
-    marginLeft: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 7,
-  },
-  errorText: {
-    color: "red",
-    marginBottom: 10,
-  },
-  button: {
-    width: "100%",
-    backgroundColor: "#007bff",
-    padding: 12,
-    alignItems: "center",
-    borderRadius: 8,
-  },
-  buttonDisabled: {
-    backgroundColor: "#5e7791",
-  },
-  buttonText: {
-    color: "black",
-    fontWeight: "bold",
-  },
-  label: {
-    fontWeight: "500",
-  },
-  dateValue: {
-    marginLeft: 8,
-    color: "#333",
-  },
-  required: {
-    color: "red",
   },
 });
